e5411e605553fbd1d4ad13e26129cc5d165e1e66
xiaoxuetong <34703535+xiaoxuetong@users.noreply.github.com>
2019-07-22 15:09:09 +0800

    Update jump.js

--- js/jump.js
+++ js/jump.js
@@ -4,6 +4,18 @@ function GetQueryString(name)
      var r= window.location.search.substr(1).match(reg);
      if(r!=null)return  unescape(r[2]);return null;
 }
+
+//////////////////////////hover on demo//////////////////////////////
+(function() {
+     function getQueryStr(name)
+     {
+          var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+          var r= window.location.search.substr(1).match(reg);
+          if(r!=null)return  unescape(r[2]);return null;
+     }
+  //var demoItems = document.querySelectorAll('.grid-item')
+     alert(getQueryStr("name"));
+}());
  
 // 调用方法
 alert(GetQueryString("name"));
