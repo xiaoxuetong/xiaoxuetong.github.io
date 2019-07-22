804756b0e41aa8b8565ebed0a0d4e08496ac5f10
xiaoxuetong <34703535+xiaoxuetong@users.noreply.github.com>
2019-07-22 15:33:45 +0800

    Update jump.js

--- js/jump.js
+++ js/jump.js
@@ -1,9 +1,9 @@
-function GetQueryString(name)
-{
-     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-     var r= window.location.search.substr(1).match(reg);
-     if(r!=null)return  unescape(r[2]);return null;
-}
+// function GetQueryString(name)
+// {
+//      var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+//      var r= window.location.search.substr(1).match(reg);
+//      if(r!=null)return  unescape(r[2]);return null;
+// }
 
 //////////////////////////hover on demo//////////////////////////////
 (function() {
@@ -14,8 +14,10 @@ function GetQueryString(name)
           if(r!=null)return  unescape(r[2]);return null;
      }
   //var demoItems = document.querySelectorAll('.grid-item')
-     alert(getQueryStr("name"));
+    var toUrl =  getQueryStr("toUrl");
+     console.log("要跳转的链接："+toUrl);
+     this.location = toUrl;
 }());
  
 // 调用方法
-alert(GetQueryString("name"));
+// alert(GetQueryString("name"));
